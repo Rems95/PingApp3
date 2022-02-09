c03874f7a66b340c333cfe1f5c3320656efe18f9
Chamossi-Dine Akanni Ghislain ABDOULAYE <chamossi-dine.abdoulaye@groupe-esigelec.org>
2022-02-09 11:19:57 +0100

    CGU

--- app/src/main/java/com/example/ping3/activities/CguActivity.java
+++ app/src/main/java/com/example/ping3/activities/CguActivity.java
@@ -5,17 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ping3.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class CguActivity extends AppCompatActivity {
+    WebView webView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cgu);
+
+        webView = findViewById(R.id.web);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("file:///android_asset/cgu.html");
+
+    }
+    private class MyBrowser extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.equals("Navigation://OpenNativeScreen")) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+                return true;
+            }
+            return false;
+        }
     }
 
     public void exit_cgu(View view){
